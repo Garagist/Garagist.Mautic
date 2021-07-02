5dad15020c1bd51891d391a9dbcce4bee3814dfc
Jon Uhlmann <me@jonnitto.ch>
2021-07-02 11:01:02 +0200

    Update: Tailwind Config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,30 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
-  mode: 'jit',
-  purge: ['Resources/Private/{Assets,Fusion}/**/*.{js,fusion}'],
+  mode: "jit",
+  purge: ["Resources/Private/{Assets,Fusion}/**/*.{js,fusion,pcss}"],
+  important: ".mautic",
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: colors.black,
+      white: colors.white,
+      gray: colors.coolGray,
+      red: colors.red,
+      yellow: colors.amber,
+      green: colors.emerald,
+    },
+    fontFamily: {
+      sans: [
+        '"Noto Sans"',
+        "sans-serif",
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+        '"Noto Color Emoji"',
+      ],
+    },
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
 };
