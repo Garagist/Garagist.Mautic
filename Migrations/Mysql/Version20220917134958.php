9ee69427656baddaaef8f98cffdda885a3c8d602
David Spiola <ds@ds-mac.local>
2022-09-17 15:55:24 +0200

    Fix: enforce unique email identifier migration

---
+++ Migrations/Mysql/Version20220917134958.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Neos\Flow\Persistence\Doctrine\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220917134958 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Enforced unique email identifier';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf(
+            !$this->connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\MariaDb1027Platform,
+            "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\MariaDb1027Platform'."
+        );
+
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_82D9319035B9BD5C ON garagist_mautic_domain_model_mauticemail (emailidentifier)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf(
+            !$this->connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\MariaDb1027Platform,
+            "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\MariaDb1027Platform'."
+        );
+
+        $this->addSql('DROP INDEX UNIQ_82D9319035B9BD5C ON garagist_mautic_domain_model_mauticemail');
+    }
+}
