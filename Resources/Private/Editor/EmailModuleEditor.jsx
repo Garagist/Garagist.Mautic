6dd3ffc837b464d1b743d9867ccea064fe71f8c5
Jon Uhlmann <account@uhlmann.pro>
2024-10-09 13:42:39 +0200

    Update: Replace build stack, write editor as a function

---
+++ Resources/Private/Editor/EmailModuleEditor.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Button, Icon, Label } from "@neos-project/react-ui-components";
+import I18n from "@neos-project/neos-ui-i18n";
+
+function EmailModuleEditor(props) {
+    const { label, className, identifier, options, renderHelpIcon, renderSecondaryInspector } = props;
+    const { src, icon, name, disabled}  = options;
+
+    return (
+        <div style={{display:"flex"}}>
+            <Label htmlFor={identifier}>
+                <Button
+                    className={className}
+                    disabled={disabled}
+                    onClick={() => {
+                        renderSecondaryInspector("IFRAME", () => (
+                            <iframe
+                                style={{ height: "100%", width: "100%", border: 0 }}
+                                name={name || "email-module"}
+                                src={
+                                    src.startsWith("ClientEval:")
+                                        ? (0, eval)(src.replace("ClientEval:", ""))
+                                        : src
+                                }
+                            />
+                        ))
+                    }}
+                    style="lighter"
+                >
+                    {icon && <Icon icon={icon} padded="right" />}
+                    <I18n id={label} />
+                </Button>
+            </Label>
+            {renderHelpIcon && renderHelpIcon()}
+        </div>
+    );
+}
+
+export default EmailModuleEditor;
---
+++ build.mjs
@@ -0,0 +1,46 @@
+import esbuild from "esbuild";
+import extensibilityMap from "@neos-project/neos-ui-extensibility/extensibilityMap.json" assert { type: "json" };
+import { cssModules } from "esbuild-plugin-lightningcss-modules";
+
+const watch = process.argv.includes("--watch");
+
+/** @type {import("esbuild").BuildOptions} */
+const globalOptions = {
+    logLevel: "info",
+    bundle: true,
+    minify: !watch,
+    sourcemap: watch,
+    target: "es2020",
+    format: "iife",
+    legalComments: "none",
+}
+
+const editorOptions = {
+    ...globalOptions,
+    entryPoints: { Plugin: "Resources/Private/Editor/manifest.js" },
+    outdir: "Resources/Public/Editor",
+    alias: extensibilityMap,
+    loader: { '.js': 'jsx' },
+    plugins: [
+        cssModules({
+            targets: {
+                chrome: 80, // aligns somewhat to es2020
+            },
+        }),
+    ],
+};
+
+const backendOptions = {
+    ...globalOptions,
+    entryPoints: ["Resources/Private/Assets/Backend.js"],
+    outfile: "Resources/Public/Scripts/Backend.js",
+}
+
+
+if (watch) {
+    esbuild.context(editorOptions).then((ctx) => ctx.watch());
+    esbuild.context(backendOptions).then((ctx) => ctx.watch());
+} else {
+    esbuild.build(editorOptions);
+    esbuild.build(backendOptions);
+}
