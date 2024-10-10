7e1b4ffcda436b14326a46b16154ad1d04918d2c
Jon Uhlmann <account@uhlmann.pro>
2024-10-10 23:02:08 +0200

    Update: Allow different types of inputs for path function

--- Classes/Service/SettingsService.php
+++ Classes/Service/SettingsService.php
@@ -6,6 +6,9 @@ namespace Garagist\Mautic\Service;
 
 use Neos\Flow\Annotations as Flow;
 use Neos\Flow\Configuration\ConfigurationManager;
+use Garagist\Mautic\Service\NodeService;
+use Neos\ContentRepository\Domain\Model\NodeInterface;
+use Garagist\Mautic\Domain\Model\MauticEmail;
 
 #[Flow\Scope('singleton')]
 class SettingsService
@@ -13,13 +16,27 @@ class SettingsService
     #[Flow\Inject]
     protected ConfigurationManager $configurationManager;
 
+    #[Flow\Inject]
+    protected NodeService $nodeService;
+
     /**
-     * Get configuration based on the setting path and sitename
+     * Get the path from the configuration
      *
+     * @param string $settingPath
+     * @param string|NodeInterface|MauticEmail|null $siteNameOrSite
+     * @param string $rootPackage
      * @return string
      */
-    public function path(string $settingPath, ?string $siteName = null, $rootPackage = 'Garagist.Mautic'): string
+    public function path(string $settingPath, $sub = null, $rootPackage = 'Garagist.Mautic'): string
     {
+        if ($sub instanceof MauticEmail) {
+            $sub = $this->nodeService->getNodeById($sub->getNodeIdentifier());
+        }
+        if ($sub instanceof NodeInterface) {
+            $sub = $this->nodeService->getSiteNameBasedOnNode($sub);
+        }
+
+        $siteName = is_string($sub) ? $sub : null;
         $siteSettings = $this->getSetting($rootPackage, $settingPath, $siteName);
 
         if (isset($siteSettings)) {
@@ -36,9 +53,9 @@ class SettingsService
      * @param string|null $siteName
      * @return mixed
      */
-    protected function getSetting(string $rootPackage, string $settingPath, ?string $siteName = null): mixed
+    protected function getSetting(string $rootPackage, string $settingPath, $siteName = null): mixed
     {
-        if ($siteName) {
+        if ($siteName && is_string($siteName)) {
             $settingPath = sprintf('siteSettings.%s.%s', $siteName, $settingPath);
         }
         if (!str_starts_with($settingPath, $rootPackage)) {
